c3f7c9ad2b6fb3a3b92bfe11c09cbfc049c5ad60
shakti-rajput <shaktirajput100@gmail.com>
2018-02-21 00:31:19 +0530

    Initial commit

---
+++ input space character.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main ()
+{
+  string mystr="";
+
+  while(1)
+  {
+  	  getline (cin, mystr);
+  	  istringstream input(mystr);
+		int value;
+		while (input >> value)
+		{
+		    cout<<value<<endl;
+		}
+		if(mystr=="")
+		break;
+  }
+  
+  cout << "I like " << mystr << " too!\n";
+  return 0;
+}
---
+++ shaktilibrary.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+void boost()
+{
+	std::ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+}
+ 
+// Utility function to find minimum of three numbers
+int min(int x, int y, int z)
+{
+   return min(min(x, y), z);
+}
